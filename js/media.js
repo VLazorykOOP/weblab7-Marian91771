3dff6d8acb775b01d82019b5714e838dbaee4008
Marian91771 <vybyranyi.marian@chnu.edu.ua>
2024-11-01 08:29:38 +0200

    дк9

--- js/media.js
+++ js/media.js
@@ -1,21 +1,18 @@
 function checkVideo() {
     const checkResult = document.getElementById("checkVideoResult");
-    
-    if (!!document.createElement('video').canPlayType) {
-        const vidTest = document.createElement("video");
-        const oggTest = vidTest.canPlayType('video/ogg; codecs="theora, vorbis"');
+    const vidTest = document.createElement("video");
 
-        if (!oggTest) {
-            const h264Test = vidTest.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
-            if (!h264Test) {
-                checkResult.innerHTML = "Вибачте. Не підтримує.";
-            } else {
-                checkResult.innerHTML = (h264Test === "probably") ? "Так!" : "Частково підтримує.";
-            }
-        } else {
-            checkResult.innerHTML = (oggTest === "probably") ? "Так!" : "Частково підтримує.";
-        }
-    } else {
+    if (!vidTest.canPlayType) {
         checkResult.innerHTML = "Вибачте. Не підтримує.";
+        return;
     }
+
+    const oggTest = vidTest.canPlayType('video/ogg; codecs="theora, vorbis"');
+    if (oggTest === "probably") {
+        checkResult.innerHTML = "Так!";
+        return;
+    }
+
+    const h264Test = vidTest.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
+    checkResult.innerHTML = (h264Test === "probably") ? "Так!" : "Не підтримує.";
 }
